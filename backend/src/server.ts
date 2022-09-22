760e9d9123bdcc95d31fcb174afc61f0c65b101c
Alisson Prates Peres <a.pratesperes@gmail.com>
2022-09-21 22:55:26 -0300

    :boom: Hello, World! no Back-End da Aplicação.
    Criada a Primeira Rota de teste de já utilizando o Express e retornando o famoso "Hello, World" no Browser, testando a Rota Raiz: "/" e a Porta: "3333", no Back-End da Aplicação.

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+    const app = express();
+
+        app.get("/", (request, response) => {
+            return response.json({
+                hello: "world"
+            });
+        });
+
+            app.listen(3333);
