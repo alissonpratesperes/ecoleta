28890a81a13835f2d590852086ba4f4b897e2052
Alisson Prates Peres <a.pratesperes@gmail.com>
2022-09-23 17:23:01 -0300

    🚚 Segunda Rota: "/points" no Back-End da Aplicação.
    Implementada a Segunda Rota: "/points", para o cadastro dos Pontos de Coleta e também para o cadastro dos Itens de Coleta, ambas operações realizadas em Tabelas Distintas;
    Não será mais utilizada a Linguagem SQL Pura devido à limitações enfrentadas e sim a Linguagem do Query Builder - KnexJS mesmo, no Back-End da Aplicação.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,22 +5,58 @@ import knex from "./database/connection";
     const routes = express.Router();
 
         routes.get("/items", async (request, response) => {
-            const items = await knex.raw(`
-                SELECT
-                    *
-                FROM
-                    items
-            `);
+            const items = await knex("items").select("*");
+
             const serializedItems = items.map((item: any) => {
                 return {
+                    id: item.id,
                     title: item.title,
-                        image_url: `http://192.168.1.101:3333/uploads/${item.image}`
+                    image_url: `http://192.168.1.101:3333/uploads/${item.image}`
                 }
             });
 
-                return response.json(
+                return response.json(  
                     serializedItems
                 );
+        });
+
+        routes.post("/points", async (request, response) => {
+            const {
+                name,
+                email,
+                whatsapp,
+                latitude,
+                longitude,
+                city,
+                uf,
+                items
+            } = request.body;
+
+            const insertedPointId = await knex("points").insert({
+                image: "image-fake.jpg",
+                name,
+                email,
+                whatsapp,
+                latitude,
+                longitude,
+                city,
+                uf
+            });
+
+            const point_id = insertedPointId[0];
+
+            const pointItems = items.map((item_id: number) => {
+                return {
+                    point_id,
+                    item_id
+                };
+            });
+
+                await knex("point_items").insert(pointItems);        
+
+                    return response.json({
+                        success: true
+                    });
         });
 
             export default routes;
