59053fc006bd90572ea0f250c1c7c2aca9360dc8
Alisson Prates Peres <a.pratesperes@gmail.com>
2022-09-23 15:49:46 -0300

    :truck: Primeira Rota: "/items" no Back-End da Aplicação.
    Implementada a Primeira Rota: "/items", para o acesso às imagens dos Itens de Coleta, que futuramente será acessada para o acesso pelo Front-End;
    Implementada a primeira Consulta SQL (será utilizada a Linguagem SQL Pura e não a Linguagem do ORM) no Banco de Dados, no Back-End da Aplicação.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,26 @@
 import express from "express";
 
+import knex from "./database/connection";
+
     const routes = express.Router();
 
-        routes.get("/", (request, response) => {
-            return response.json({
-                message: "Hello, World!"
+        routes.get("/items", async (request, response) => {
+            const items = await knex.raw(`
+                SELECT
+                    *
+                FROM
+                    items
+            `);
+            const serializedItems = items.map((item: any) => {
+                return {
+                    title: item.title,
+                        image_url: `http://192.168.1.101:3333/uploads/${item.image}`
+                }
             });
+
+                return response.json(
+                    serializedItems
+                );
         });
 
             export default routes;
