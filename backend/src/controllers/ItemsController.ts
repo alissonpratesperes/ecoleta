b1665bb4ace88545a0421dded035df318419bc39
Alisson Prates Peres <a.pratesperes@gmail.com>
2022-09-24 14:38:35 -0300

    🏗️ Abstração da Rota: "/items" no Back-End da Aplicação.
    Realizada a abstração da Rota: "/items" para o novo Controller em "./backend/src/controllers/ItemsController.ts", no Back-End da Aplicação.

---
+++ backend/src/controllers/ItemsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+import knex from "../database/connection";
+
+    class ItemsController {
+        async index(request: Request, response: Response) {
+            const items = await knex("items").select("*");
+
+            const serializedItems = items.map((item: any) => {
+                return {
+                    id: item.id,
+                    title: item.title,
+                    image_url: `http://192.168.1.101:3333/uploads/${item.image}`
+                }
+            });
+
+                return response.json(  
+                    serializedItems
+                );
+        }
+    }
+
+        export default ItemsController;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,27 +1,13 @@
 import express from "express";
 
-import knex from "./database/connection";
 import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
     const routes = express.Router();
     const pointsController = new PointsController();
+    const itemsController = new ItemsController();
 
-        routes.get("/items", async (request, response) => {
-            const items = await knex("items").select("*");
-
-            const serializedItems = items.map((item: any) => {
-                return {
-                    id: item.id,
-                    title: item.title,
-                    image_url: `http://192.168.1.101:3333/uploads/${item.image}`
-                }
-            });
-
-                return response.json(  
-                    serializedItems
-                );
-        });
-
+        routes.get("/items", itemsController.index);
         routes.post("/points", pointsController.create);
 
             export default routes;
